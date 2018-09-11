248f4bd8cc50f427e924c6787d4d08553dbe375d
810230010 <810230010@qq.com>
2018-09-11 11:44:39 +0800

    重构

--- rpc-lite/rpc-core/src/main/java/com/usher/rpc/common/serialization/Serializor.java
+++
@@ -1,9 +0,0 @@
-package com.usher.rpc.common.serialization;
-
-import java.io.Serializable;
-
-public abstract class Serializor {
-    public abstract <T extends Serializable> byte[] serialize(T src);
-
-    public abstract <T extends Serializable> Object deserialize(byte[] src, Class<T> clazz);
-}
--- rpc-lite/rpc-lite-example/rpc-lite-example-api/src/main/java/com/user/rpc/api/IUserService.java
+++
@@ -1,5 +0,0 @@
-package com.user.rpc.api;
-
-public interface IUserService {
-    String sayHi();
-}
